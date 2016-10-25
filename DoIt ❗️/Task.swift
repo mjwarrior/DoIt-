18792bef6fad5073519baf0228015132dd2576d4
marcstreeter <mstreeter@gmail.com>
2016-10-25 00:33:11 -0500

    if important add ❗️ commit

---
+++ DoIt ❗️/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt ❗️
+//
+//  Created by Marc Streeter on 10/25/16.
+//  Copyright © 2016 Developer Warriors. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
